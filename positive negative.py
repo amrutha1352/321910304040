fecc0c27d57cde1c5307b7572c32cc26c9c5daff
amrutha1352 <67035752+amrutha1352@users.noreply.github.com>
2020-07-09 12:31:15 +0530

    Add files via upload

---
+++ positive negative.py
@@ -0,0 +1,9 @@
+In [12]:
+num=int(input('enter a number'))
+if(num==0):
+  print("num is zero")
+else:
+  if num>0:
+   print("num is positive")
+  else:
+   print("num is negative")
---
+++ square root.py
@@ -0,0 +1,4 @@
+In [4]:
+num=float(input('enter a number to find the square root'))
+num_sqrt=num**0.5
+print(num_sqrt)
